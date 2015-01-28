ebfe152295e8e80a2e7a8c97759b463bd8a4a34d
Adam Starbuck <acs11e@acu.edu>
2015-01-28 14:55:04 -0600

    Deleted old files that I was using for testing purposes

--- coroniumIOtest/answer.lua
+++
@@ -1,7 +0,0 @@
--- Collect incoming data
-local in_data = coronium.input()
-
--- local answer = coronium.mongo:createObject( "times", { startTime = in_data.startTime } )
-local answer = coronium.mongo:getObject( "times", in_data.objectid ) 
-
-coronium.output( answer )
--- timerTesting/develephant-coronium-gs-bundle-675784888054/readme.md
+++
@@ -1,3 +0,0 @@
-## Download ##
-
- __[Click here to download the latest Coronium GS Bundle](https://bitbucket.org/develephant/coronium-gs-bundle/get/default.zip)__.
